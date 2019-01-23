96e39381e2ae1a7f43e6085d5a73128430146318
santoshchewale <santosh.chewale@gmail.com>
2019-01-23 09:27:38 +0530

    First commit

---
+++ gateway-service/src/main/java/com/zensar/edge/Application.java
@@ -0,0 +1,24 @@
+package com.zensar.edge;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+//@EnableOAuth2Sso
+//@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+	@Bean
+	public AlwaysSampler defaultSampler() {
+	  return new AlwaysSampler();
+	}
+	
+}
